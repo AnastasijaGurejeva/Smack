597b58f74de17779f5a1548981896056e20896d1
Anastasija Gurejeva <anastasija@Faizhals-MBP.net>
2019-07-10 15:22:36 +0200

    Allert dialog for channel created

--- app/src/main/java/com/example/smack/Controller/MainActivity.kt
+++ app/src/main/java/com/example/smack/Controller/MainActivity.kt
@@ -1,18 +1,18 @@
 package com.example.smack.Controller
 
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
+import android.content.*
 import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.content.LocalBroadcastManager
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import com.example.smack.R
 import com.example.smack.Services.AuthService
 import com.example.smack.Services.UserDataService
@@ -38,15 +38,18 @@ class MainActivity : AppCompatActivity() {
         )
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
+        hideKeyboard()
 
-        LocalBroadcastManager.getInstance(this).registerReceiver(userDataChangeReceiver,
-            IntentFilter(BROADCAST_USER_DATA_CHANGES))
+        LocalBroadcastManager.getInstance(this).registerReceiver(
+            userDataChangeReceiver,
+            IntentFilter(BROADCAST_USER_DATA_CHANGES)
+        )
     }
 
-    private val userDataChangeReceiver = object: BroadcastReceiver() {
+    private val userDataChangeReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             println(AuthService.isLoggedIn)
-            if(AuthService.isLoggedIn) {
+            if (AuthService.isLoggedIn) {
                 userNameNavHeader.text = UserDataService.name
                 println(UserDataService.name)
                 user_email_nav_header.text = UserDataService.email
@@ -69,7 +72,25 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onAddChannelClicked(view: View) {
+        if (AuthService.isLoggedIn) {
+            val builder = AlertDialog.Builder(this)
+            val dialogView = layoutInflater.inflate(R.layout.add_channel_dialog, null)
+            builder.setView(dialogView)
+                .setPositiveButton("Add") { dialog: DialogInterface?, which: Int ->
+                    val nameTextField = dialogView.findViewById<EditText>(R.id.addChannelNameTxt)
+                    val descTextField = dialogView.findViewById<EditText>(R.id.addChannelDescTxt)
+                    val channelName = nameTextField.text.toString()
+                    val channelDescription = descTextField.text.toString()
 
+                    hideKeyboard()
+
+                }
+                .setNegativeButton("Cancel") { dialog: DialogInterface?, which: Int ->
+
+                    hideKeyboard()
+                }
+                .show()
+        }
     }
 
     fun onLoginClicked(view: View) {
@@ -89,7 +110,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onSendMessageClicked(view: View) {
-        
+
+    }
+
+    fun hideKeyboard() {
+        val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        if (inputManager.isAcceptingText) {
+            inputManager.hideSoftInputFromWindow(currentFocus.windowToken, 0)
+        }
     }
 
 
